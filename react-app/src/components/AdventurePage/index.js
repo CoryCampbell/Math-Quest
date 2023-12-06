4158bb692456648f0f2324a37236bec27d90e2fa
CoryCampbell <corycampbell20@yahoo.com>
2023-12-06 11:23:12 -0600

    css changes to adventure page

--- react-app/src/components/AdventurePage/index.js
+++ react-app/src/components/AdventurePage/index.js
@@ -6,15 +6,28 @@ function AdventurePage() {
 	const sessionUser = useSelector((state) => state.session.user);
 
 	if (!sessionUser) return <Redirect to="/" />;
+
+	function startAdditionAdventure() {
+		console.log("addition adventure started!");
+	}
+
 	return (
 		<>
 			<div className="adventure-page-container">
-				<div className="page-title">Adventures Page</div>
+				<div className="page-title">Select Your Adventure!</div>
 				<div className="adventure-options-container">
-					<div className="adventure-option">Addition</div>
-					<div className="adventure-option">Subtraction</div>
-					<div className="adventure-option">Multiplication</div>
-					<div className="adventure-option">Division</div>
+					<button className="adventure-option add" onClick={startAdditionAdventure}>
+						Addition Adventure
+					</button>
+					<button className="adventure-option sub" disabled>
+						Subtraction Adventure Coming Soon!
+					</button>
+					<button className="adventure-option mult" disabled>
+						Multiplication Adventure Coming Soon!
+					</button>
+					<button className="adventure-option division" disabled>
+						Division Adventure Coming Soon!
+					</button>
 				</div>
 			</div>
 		</>
