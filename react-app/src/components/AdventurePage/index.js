c007d791e776dc38ce0ebbe1cff8729ba7d0b6d5
CoryCampbell <corycampbell20@yahoo.com>
2023-12-06 11:36:02 -0600

    css changes to village page

--- react-app/src/components/AdventurePage/index.js
+++ react-app/src/components/AdventurePage/index.js
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import "./AdventurePage.css";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 function AdventurePage() {
 	const sessionUser = useSelector((state) => state.session.user);
+	const selectedCharacter = useSelector((state) => state.characters.selectedCharacter);
 
 	if (!sessionUser) return <Redirect to="/" />;
 
@@ -13,23 +15,30 @@ function AdventurePage() {
 
 	return (
 		<>
-			<div className="adventure-page-container">
-				<div className="page-title">Select Your Adventure!</div>
-				<div className="adventure-options-container">
-					<button className="adventure-option add" onClick={startAdditionAdventure}>
-						Addition Adventure
-					</button>
-					<button className="adventure-option sub" disabled>
-						Subtraction Adventure Coming Soon!
-					</button>
-					<button className="adventure-option mult" disabled>
-						Multiplication Adventure Coming Soon!
-					</button>
-					<button className="adventure-option division" disabled>
-						Division Adventure Coming Soon!
-					</button>
+			{selectedCharacter ? (
+				<div className="adventure-page-container">
+					<div className="page-title">Select Your Adventure!</div>
+					<div className="adventure-options-container">
+						<button className="adventure-option add" onClick={startAdditionAdventure}>
+							Addition Adventure
+						</button>
+						<button className="adventure-option sub" disabled>
+							Subtraction Adventure Coming Soon!
+						</button>
+						<button className="adventure-option mult" disabled>
+							Multiplication Adventure Coming Soon!
+						</button>
+						<button className="adventure-option division" disabled>
+							Division Adventure Coming Soon!
+						</button>
+					</div>
 				</div>
-			</div>
+			) : (
+				<div className="no-character-adv-page">
+					<div>Please Select A Character To Start A New Adventure!</div>
+					<NavLink to="/characters">Characters</NavLink>
+				</div>
+			)}
 		</>
 	);
 }
