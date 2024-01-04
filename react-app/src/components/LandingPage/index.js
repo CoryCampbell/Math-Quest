81487fdffb0b4d0c8ba624b7110c1d88597fb136
CoryCampbell <corycampbell20@yahoo.com>
2024-01-03 20:51:23 -0600

    WED b4 gym

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -12,31 +12,39 @@ function LandingPage() {
 		<>
 			<div className="landing-page-container">
 				<h1>Welcome to MathQuest</h1>
-				<img
-					src={require("../../static/landing/adventure-preview.png").default}
-					alt="adv-preview"
-					className="adv-prev"
-				></img>
-				<img
-					src={require("../../static/landing/characters-preview.png").default}
-					alt="char-preview"
-					className="char-prev"
-				></img>
-				<img
-					src={require("../../static/landing/village-preview.png").default}
-					alt="village-preview"
-					className="village-prev"
-				></img>
+
 				<h4>"Mathematics is not a language. It's an adventure." - Paul Lockhart</h4>
 				<div className="landing-page-details">
-					<div className="landing-detail-one-container">
-						<div className="detail-one-text">Solve Math Problems To Complete Adventures!</div>
+					<div className="prev-one">
+						<img
+							src={require("../../static/landing/adventure-preview.png").default}
+							alt="adv-preview"
+							className="adv-prev"
+						></img>
+						<div className="landing-detail-one-container">
+							<div className="detail-one-text">Solve Math Problems To Complete Adventures!</div>
+						</div>
 					</div>
-					<div className="landing-detail-two-container">
-						<div className="detail-two-text">Gain Experience And Level Up Your Character!</div>
+					<div className="prev-two">
+						<img
+							src={require("../../static/landing/characters-preview.png").default}
+							alt="char-preview"
+							className="char-prev"
+						></img>
+						<div className="landing-detail-two-container">
+							<div className="detail-two-text">Gain Experience And Level Up Your Character!</div>
+						</div>
 					</div>
-					<div className="landing-detail-three-container">
-						<div className="detail-three-text">Compete with Others For The #1 Spot On The Leaderboard!</div>
+
+					<div className="prev-three">
+						<img
+							src={require("../../static/landing/village-preview.png").default}
+							alt="village-preview"
+							className="village-prev"
+						></img>
+						<div className="landing-detail-three-container">
+							<div className="detail-three-text">Compete with Others For The #1 Spot On The Leaderboard!</div>
+						</div>
 					</div>
 				</div>
 			</div>
