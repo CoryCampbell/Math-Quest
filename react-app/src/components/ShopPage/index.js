395884558393ad5a60b4f557dc77904703a70316
CoryCampbell <corycampbell20@yahoo.com>
2024-01-09 22:19:47 -0600

    shop page css styling

--- react-app/src/components/ShopPage/index.js
+++ react-app/src/components/ShopPage/index.js
@@ -4,12 +4,23 @@ import "./ShopPage.css";
 
 function ShopPage() {
 	const sessionUser = useSelector((state) => state.session.user);
+	const currentAdventure = localStorage.getItem("currentAdventure") || {};
 
 	if (!sessionUser) return <Redirect to="/" />;
+
+	if (Object.keys(currentAdventure).length !== 0) {
+		alert("You are on an adventure! You can't use the shop right now!");
+
+		return <Redirect to="/adventures" />;
+	}
 	return (
 		<>
 			<div className="shop-page-container">
-				<h1>Shop Coming Soon!</h1>
+				<div className="shop-left">L</div>
+				<div className="shop-right">
+					<div className="shop-item-preview">Preview</div>
+					<button className="buy button">Buy</button>
+				</div>
 			</div>
 		</>
 	);
