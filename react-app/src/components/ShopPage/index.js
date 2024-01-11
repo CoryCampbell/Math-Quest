bef8ec0e28b8a3fb69002610a5a49aade0437535
CoryCampbell <corycampbell20@yahoo.com>
2024-01-10 19:08:52 -0600

    shop setup

--- react-app/src/components/ShopPage/index.js
+++ react-app/src/components/ShopPage/index.js
@@ -16,10 +16,26 @@ function ShopPage() {
 	return (
 		<>
 			<div className="shop-page-container">
-				<div className="shop-left">L</div>
-				<div className="shop-right">
-					<div className="shop-item-preview">Preview</div>
-					<button className="buy button">Buy</button>
+				<h3>POTIONS</h3>
+				<div className="potions-section">
+					<div className="small-potion shop-item">Small Potion</div>
+					<div className="medium-potion shop-item">Medium Potion</div>
+					<div className="large-potion shop-item">Large Potion</div>
+				</div>
+				<h3>WEAPONS</h3>
+				<div className="weapons-section">
+					<div className="bow-arrows shop-item">Bow & arrows</div>
+					<div className="sword shop-item">Sword</div>
+					<div className="shield shop-item">Shield</div>
+					<div className="glaive shop-item">Glaive</div>
+					<div className="staff shop-item">Staff</div>
+				</div>
+				<h3>ARMOR</h3>
+				<div className="armor-section">
+					<div className="steel-armor shop-item">Steel Armor</div>
+					<div className="golden-armor shop-item">Golden Armor</div>
+					<div className="netherite-armor shop-item">Netherite Armor</div>
+					<div className="obsidian-armor shop-item">Obsidian Armor</div>
 				</div>
 			</div>
 		</>
